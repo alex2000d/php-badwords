466d3b1ae1afc6603584b13cdad2e162ea2c7a27
alex2000d <demarcoalessandro777@gmail.com>
2024-09-04 15:15:45 +0200

    [UPD] created the input and button

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="form-action" method="GET">
+                     <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <div class="control-label">parola</div>
+                                <input type="text" class="form-control" name="parola" placeholder="inserisci parola">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <div class="control-label">paragrafo</div>
+                                <input type="text" class="form-control" name="paragrafo" placeholder="inserisci paragrafo">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">invia</button>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
